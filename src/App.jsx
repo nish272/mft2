909d03a5abe3a362587049f7ea8c076cbaa74788
nish272 <chaudharynisha497@gmail.com>
2025-03-02 21:01:08 +0530

    a

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Movie from './Movie.jsx';
+
+function App() {
+  return (
+    <>
+      <h1></h1>
+      <Movie />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Movie.jsx
@@ -0,0 +1,130 @@
+import { useState, useEffect } from 'react';
+import './Movie.css';
+import { AiOutlineSearch } from 'react-icons/ai';
+import axios from 'axios';
+
+function Movie() {
+  const [movies, setMovies] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [sortBy, setSortBy] = useState('popularity.desc');
+  const [selectedGenre, setSelectedGenre] = useState('');
+  const [genres, setGenres] = useState([]);
+  const [expandedMovieId, setExpandedMovieID] = useState(null);
+
+  useEffect(() => {
+    const fetchGenres = async () => {
+      const response = await axios.get(
+        'https://api.themoviedb.org/3/genre/movie/list',
+        {
+          params: {
+            api_key: 'cb948cf5672c0e16914a858761a6caa0',
+          },
+        }
+      );
+      setGenres(response.data.genres);
+      console.log(response.data.genres);
+    };
+    fetchGenres();
+  }, []);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await axios.get(
+        'https://api.themoviedb.org/3/discover/movie',
+        {
+          params: {
+            api_key: 'cb948cf5672c0e16914a858761a6caa0',
+            query: searchQuery,
+            sort_by: sortBy,
+            with_genres: selectedGenre,
+            page: 1,
+          },
+        }
+      );
+      setMovies(response.data.results);
+    };
+    fetchMovies();
+  }, [searchQuery, sortBy, selectedGenre]);
+
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSearchSubmit = async () => {
+    const response = await axios.get(
+      'https://api.themoviedb.org/3/search/movie',
+      {
+        params: {
+          api_key: 'cb948cf5672c0e16914a858761a6caa0',
+          query: searchQuery,
+          sort_by: sortBy,
+          with_genres: selectedGenre,
+        },
+      }
+    );
+    setMovies(response.data.results);
+  };
+
+  const handleSortChange = (event) => {
+    setSortBy(event.target.value);
+  };
+
+  const handleGenreChange = (event) => {
+    setSelectedGenre(event.target.value);
+  };
+
+  const toggleDescription = (movieId) => {
+    setExpandedMovieID(expandedMovieId === movieId ? null : movieId);
+  };
+
+  return (
+    <div>
+      <h1><strong>MOVIE HOUSE</strong></h1>
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search Movies..."
+          value={searchQuery}
+          onChange={handleSearchChange}
+          className="search-input"
+        />
+        <button onClick={handleSearchSubmit} className="search-button">
+          <AiOutlineSearch />
+        </button>
+      </div>
+      <div className="filters">
+        <label htmlFor="sort-by">Sort By:</label>
+        <select id="sort-by" value={sortBy} onChange={handleSortChange}>
+          <option value="popularity.desc">Popularity Descending</option>
+          <option value="popularity.asc">Popularity Ascending</option>
+          <option value="vote_average.desc">Rating Descending</option>
+          <option value="vote_average.asc">Rating Ascending</option>
+          <option value="release_date.desc">Release Date Descending</option>
+          <option value="release_date.asc">Release Date Ascending</option>
+        </select>
+        <label htmlFor="genre">Genre:</label>
+        <select id="genre" value={selectedGenre} onChange={handleGenreChange}>
+          <option value="">All Genres</option>
+          {genres.map((genre) => (
+            <option key={genre.id} value={genre.id}>
+              {genre.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="movie-wrapper">
+        {movies.map((movie) => (
+          <div key={movie.id} className="movie-item">
+            <img  className='movie-image' src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
+            <h2>{movie.title}</h2>
+            <p>{expandedMovieId === movie.id ? movie.overview : `${movie.overview.substring(0, 150)}...`}</p>
+            <button onClick={() => toggleDescription(movie.id)} className="read-more">
+              {expandedMovieId === movie.id ? 'Show Less' : 'Read More'}
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+export default Movie;
