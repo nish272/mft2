13603283b5c9563743809cc15bb8e7a78a2f99f8
nish272 <chaudharynisha497@gmail.com>
2025-07-10 17:01:42 +0530

    Update Movie.jsx and CSS for styling fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/movie-house-nisha/"
+  plugins: [react()]
+  // base: "/movie-house-nisha/"  ← isko hata do
 });
